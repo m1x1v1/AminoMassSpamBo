22d251c2d7ea37ac1fcca12e7a3289b323ba43f4
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-04-10 21:47:11 +0800

    Add files via upload

---
+++ AminoMassSpamBo.py
@@ -0,0 +1,59 @@
+﻿from colorama import init
+from colorama import Fore, Back, Style
+init()
+import time
+import os
+from threading import Thread
+print(Back.BLACK)
+print(Fore.BLUE)
+print("Script by Zevi/Скрипт сделан Zevi")
+print("┌────────────────────────────────────┐")
+print("│Author :  LilZevi                   │")
+print("│Github : https://github.com/LilZevi │")
+print("└────────────────────────────────────┘")
+print("YouTube: https://www.youtube.com/channel/UCJ61JlXJckmO6yJr8BDRuGQ")
+print("▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █▄░▄█ ▄▀▀ ▄▀▀ █▀▄ ▄▀▄")
+print("█▀█ █░█░█ █ █░▀█ █░█ █░█░█ ░▀▄ ░▀▄ █▀█ █░█")
+print("▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀░░░▀ ▀▀░ ▀▀░ ▀▀░ ░▀░")
+import amino
+from amino import Client
+r = 0
+email=input("Email/Почта:")
+password=input('Password/Пароль:')
+client = amino.Client()
+client.login(email=email, password=password)
+for name, id in zip(client.sub_clients().name, client.sub_clients().comId):
+    print(f"{name}: {id}")
+    
+comid = str(input("Выберите сообщество(id): "))
+#  == config == 
+
+msgSpam=input('Message/Сообщение:')
+print('MessageTypes/Типы сообщений:Default-0,Transparent-110/100/115/108')
+msgType=input('MessageType/Тип Сообщения:')
+
+r = 400;
+
+# == functions ==
+
+def spam(sub_client, chatId, msgSpam, msgType):
+	while True:
+		try: sub_client.send_message(message=msgSpam, chatId=chatId, messageType=msgType)
+		except: pass
+
+def start_thread(sub_client, chatId, msgSpam, msgType):
+	global r;
+	for i in range(r):
+		Thread(target=spam, args=(sub_client, chatId, msgSpam, msgType)).start();
+        
+
+# == source    ==
+
+sub_client = amino.SubClient(comId=str(comid), profile=client.profile)
+
+threads_list = sub_client.get_chat_threads().json;
+
+for t in threads_list:
+	Thread(target=start_thread, args=(sub_client, t["threadId"], msgSpam, msgType)).start();
+
+print("Spamming in all chats/Спамим во всех чатах!");
